ced8153e84598ea3ab7bddefc6362d4fba1b355f
Miray Buyukkaray <miraybuyukkaray@gmail.com>
2024-01-23 21:06:26 +0100

    exploring vue

---
+++ src/components/TagsInput.vue
@@ -0,0 +1,12 @@
+<template>
+  <div v-text="tags.length"></div>
+  <input type="text" />
+</template>
+
+<script>
+export default {
+  data: () => ({
+    tags: ["vue", "react", "angular"],
+  }),
+};
+</script>
