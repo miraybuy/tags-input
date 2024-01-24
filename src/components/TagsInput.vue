bfa7e4601ee76de051d1335ebb75f10670318be9
Miray Buyukkaray <miraybuyukkaray@gmail.com>
2024-01-24 11:56:11 +0100

    event handling

--- src/components/TagsInput.vue
+++ src/components/TagsInput.vue
@@ -1,12 +1,18 @@
 <template>
-  <div v-text="tags.length"></div>
-  <input type="text" />
+  <div v-for="(tag, index) in tags">{{ index + ":" + tag }}</div>
+  <input
+    type="text"
+    :value="newTag"
+    v-on:keydown.enter="tags.push($event.target.value)"
+    v-on:keydown.tab.prevent="tags.push($event.target.value)"
+  />
 </template>
 
 <script>
 export default {
   data: () => ({
     tags: ["vue", "react", "angular"],
+    newTag: "preact",
   }),
 };
 </script>
